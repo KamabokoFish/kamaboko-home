d7e94aa214e22e4c247b6f11ddb39056c4d7a9f8
kamaboko <kadoma.fukakusa248@gmail.com>
2024-10-21 18:44:14 +0900

    refactor(prettier): set prettier as npm command,disable eslint-formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next/core-web-vitals', 'next/typescript'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
